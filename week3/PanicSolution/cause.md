ade60a63a8480804e7f6eca5bf5677d25dc6619a
來紀 <490719815@qq.com>
2024-10-31 19:04:09 +0800

    panic? recover!

---
+++ week3/PanicSolution/cause.md
@@ -0,0 +1,15 @@
+```
+if len(consumeMSG) >= ConsumeNum {
+          //进行异步消费
+          go func() {
+             m := consumeMSG[:ConsumeNum]
+             fn(m)
+          }()
+          // 更新上次消费时间
+          lastConsumeTime = time.Now()
+          // 清除插入的数据
+          consumeMSG = consumeMSG[ConsumeNum:]
+```
+
+**异步消费处理和更新消费时间，清除插入数据不同步，消费在新 goroutine，更新，插入在主 goroutine**
+另一情况同上
---
+++ week3/PanicSolution/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type message struct {
+	Topic     string
+	Partition int32
+	Offset    int64
+}
+
+type FeedEventDM struct {
+	Type    string
+	UserID  int
+	Title   string
+	Content string
+}
+
+var wg sync.WaitGroup
+
+type MSG struct {
+	ms        message
+	feedEvent FeedEventDM
+}
+
+const ConsumeNum = 5
+
+func fn(m []MSG) {
+	fmt.Printf("本次消费了%d条消息\n", len(m))
+}
+
+func main() {
+	var consumeMSG []MSG
+	var lastConsumeTime time.Time
+	msgs := make(chan MSG)
+	go func() {
+		for i := 0; ; i++ {
+			//wg.Add(1)
+			//defer wg.Done()
+			msgs <- MSG{ms: message{Topic: "消费主题", Partition: 0, Offset: 0}, feedEvent: FeedEventDM{Type: "grade", UserID: i, Title: "成绩提醒", Content: "您的成绩是xxx"}}
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	for msg := range msgs {
+		consumeMSG = append(consumeMSG, msg)
+		if len(consumeMSG) >= ConsumeNum {
+			go func() {
+				m := consumeMSG[:ConsumeNum]
+				fn(m)
+				lastConsumeTime = time.Now()
+				consumeMSG = consumeMSG[ConsumeNum:]
+			}()
+			//lastConsumeTime = time.Now()
+			//	consumeMSG = consumeMSG[ConsumeNum:]
+		} else if !lastConsumeTime.IsZero() && time.Since(lastConsumeTime) > 5*time.Minute {
+			if len(consumeMSG) > 0 {
+				go func() {
+					m := consumeMSG[:ConsumeNum]
+					fn(m)
+					lastConsumeTime = time.Now()
+					consumeMSG = consumeMSG[ConsumeNum:]
+				}()
+				//lastConsumeTime = time.Now()
+				//consumeMSG = consumeMSG[ConsumeNum:]
+			}
+		}
+	}
+	//wg.Wait()
+}
